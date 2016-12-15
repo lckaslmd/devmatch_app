0f98844a027be24d8d09a556b2cd0bbb8a4e8aff
Lucas Almeida <lucas.almdsz@outlook.com>
2016-12-15 03:55:10 -0200

    Fixed js for stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    devise_for :users
     root to: 'pages#home'
+    devise_for :users, controllers: { registrations: 'users/registrations' }
     get 'about', to: 'pages#about' # pages controller - about action
     resources :contacts, only: :create # was [:new, :create], but would have two 'new'
     get 'contact-us', to: 'contacts#new', as: 'new_contact'
